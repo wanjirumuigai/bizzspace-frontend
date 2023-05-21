6f9897ad5d7be083e3ea991b42197307e9473db8
Eve-Kamau <evelyne.kamau@student.moringaschool.com>
2023-05-21 21:55:52 +0300

    Commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,10 @@
 import "../App.css";
+import NavBar from "./NavBar";
 
 function App() {
   return (
     <div className="App">
+      <NavBar />
       <header className="App-header">
         <h1>BizzSpace</h1>
       </header>
---
+++ src/components/NavBar.js
@@ -0,0 +1,35 @@
+import { NavLink } from 'react-router-dom'
+import Logo from './bizlogo.png'
+
+const Navbar = () => {
+return (
+    <nav className="navbar">
+      <div className="container">
+      <div className="navbar-brand">
+			<img src= {Logo} alt="logo"/>
+	   </div>
+        <div className="nav-elements">
+          <ul>
+            <li>
+              <NavLink className="nav-link" to="/">Home</NavLink>
+            </li>
+            <li>
+              <NavLink className="nav-link" to="/about">About</NavLink>
+            </li>
+            <li>
+              <NavLink className="nav-link" to="/property">Add A Property</NavLink>
+            </li>
+            <li>
+              <NavLink className="nav-link" to="/contact">Contact</NavLink>
+            </li>
+            <li>
+              <NavLink className="nav-link" to="/signup">Sign Up / Log In</NavLink>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  )
+}
+export default Navbar
+
