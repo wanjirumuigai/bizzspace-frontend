be3a89503e804c5bc9b7f42bc5733fd9336b9e31
Pauline Muigai <wanjirumuigai@gmail.com>
2023-05-23 11:21:30 +0300

    added delete, book, close buttons

--- src/components/ViewOneSpace.js
+++ src/components/ViewOneSpace.js
@@ -2,14 +2,19 @@ import React, { useEffect, useState } from "react";
 import { styled } from "@mui/material/styles";
 import Typography from "@mui/material/Typography";
 import {
+  Alert,
   Box,
   Button,
   ButtonBase,
   Card,
   CardActions,
   CardContent,
+  FormControlLabel,
+  FormLabel,
   Grid,
   Paper,
+  Radio,
+  RadioGroup,
   Rating,
   TextField,
 } from "@mui/material";
@@ -20,8 +25,11 @@ export default function ViewOneSpace() {
   const [value, setValue] = useState([]);
   const rating_average = value.reduce((a, b) => a + b, 0) / value.length;
   const [rating, setRating] = useState(0);
-  const [hover, setHover] = useState(0);
+
+  const [comment, setComments] = useState([]);
   const [review, setReview] = useState("");
+  const [classname, updateClass] = useState(false);
+  const spaceBookedClass = classname ? "booked" : "";
   const Img = styled("img")({
     margin: "auto",
     display: "block",
@@ -35,15 +43,33 @@ export default function ViewOneSpace() {
       .then((data) => {
         setSpaces(data.space);
         setValue(data.space.reviews.map((item) => item.rating));
+        setComments(data.space.reviews.map((item) => item.comment));
       });
   }, []);
 
   function handleBooking() {
-    console.log(spaces);
+    updateClass((current) => !current);
+    console.log(classname);
+    if (classname == false) {
+      document.querySelector("#booking").disabled = true;
+      document.querySelector("#booking").className = "booked";
+    }
+    fetch(`/spaces/${id}`, {
+      method: "PATCH",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ is_taken: true, leased_by_id: 2 }),
+    })
+      .then((res) => res.json())
+      .then((data) => console.log(data));
   }
 
   function handleChange(e) {
     setReview(e.target.value);
+  }
+  function handleRating(e) {
+    setRating(e.target.value);
   }
   function handleSubmit() {
     console.log(review, rating);
@@ -60,7 +86,11 @@ export default function ViewOneSpace() {
       }),
     })
       .then((res) => res.json())
-      .then((data) => data);
+      .then((data) => setComments(...comment, data));
+  }
+
+  function handleDelete() {
+    alert("are you sure?");
   }
 
   return (
@@ -75,6 +105,8 @@ export default function ViewOneSpace() {
             theme.palette.mode === "dark" ? "#1A2027" : "#fff",
         }}
       >
+        <Button size="small">DELETE</Button>
+        <Button size="small">CLOSE</Button>
         <Grid container spacing={2}>
           <Grid item>
             <ButtonBase sx={{ width: 700, height: 350 }}>
@@ -97,13 +129,27 @@ export default function ViewOneSpace() {
               <Typography variant="body2" color="text.secondary">
                 Kshs. {spaces.lease_cost}
               </Typography>
-              <Typography variant="body2" color="text.secondary">
-                {spaces.is_taken ? (
-                  "Not Available"
-                ) : (
-                  <Button onClick={handleBooking}>Book</Button>
-                )}
+              <Typography
+                variant="body2"
+                color="text.secondary"
+                className={spaceBookedClass}
+              >
+                {/* <Button className="add" onClick={handleBooking} id="booking">
+                  {spaces.is_taken ? "Not Available" : "Book"}
+                </Button> */}
               </Typography>
+
+              <Button
+                variant="contained"
+                color="success"
+                onClick={handleBooking}
+                id="booking"
+              >
+                {spaces.is_taken ? "Not Available" : "Book"}
+              </Button>
+              <Button variant="outlined" color="error" onClick={handleDelete}>
+                Delete Listing
+              </Button>
             </Grid>
             <Grid item></Grid>
           </Grid>
@@ -121,6 +167,7 @@ export default function ViewOneSpace() {
           }}
         >
           <TextField
+            required
             fullWidth
             label="Post a review"
             id="fullWidth"
@@ -128,31 +175,48 @@ export default function ViewOneSpace() {
           />
         </Box>
         <Box>
-          Rate the property
-          <div className="star-rating">
-            {[...Array(5)].map((star, index) => {
-              index += 1;
-              return (
-                <button
-                  type="button"
-                  key={index}
-                  className={index <= (hover || rating) ? "on" : "off"}
-                  onClick={() => setRating(index)}
-                  onMouseEnter={() => setHover(index)}
-                  onMouseLeave={() => setHover(rating)}
-                >
-                  <span className="star">&#9733;</span>
-                </button>
-              );
-            })}
-          </div>
+          <Typography variant="body2" color="text.secondary">
+            Rate the Property
+          </Typography>
+
+          <RadioGroup
+            row
+            aria-labelledby="demo-row-radio-buttons-group-label"
+            name="row-radio-buttons-group"
+            onChange={handleRating}
+          >
+            <FormControlLabel
+              value="1"
+              control={<Radio size="small" />}
+              label="1"
+            />
+            <FormControlLabel
+              value="2"
+              control={<Radio size="small" />}
+              label="2"
+            />
+            <FormControlLabel
+              value="3"
+              control={<Radio size="small" />}
+              label="3"
+            />
+            <FormControlLabel
+              value="4"
+              control={<Radio size="small" />}
+              label="4"
+            />
+            <FormControlLabel
+              value="5"
+              control={<Radio size="small" />}
+              label="5"
+            />
+          </RadioGroup>
         </Box>
         <Box>
           <Button onClick={handleSubmit}>Submit</Button>
         </Box>
         <Card sx={{ minWidth: 150 }}>
           <CardContent>
-            <Rating name="read-only" value={rating_average} readOnly />
             <Typography
               sx={{ fontSize: 14 }}
               color="text.secondary"
@@ -161,11 +225,16 @@ export default function ViewOneSpace() {
               Reviews
             </Typography>
             <Typography variant="h5" component="div"></Typography>
-            <Typography sx={{ mb: 1.5 }} color="text.secondary">
-              {spaces.reviews.map((item) => {
-                return <ul> {item.comment} </ul>;
+            <Typography
+              sx={{ mb: 1.5 }}
+              color="text.secondary"
+              backgroundColor="lightgrey"
+            >
+              {comment.map((item) => {
+                return <p> {item} </p>;
               })}
             </Typography>
+            <Rating name="read-only" value={rating_average} readOnly />
           </CardContent>
         </Card>
       </Paper>
