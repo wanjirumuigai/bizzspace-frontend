1f9d133ca19f83461985e809d5e3c3b2ddf07e33
Charles Swaleh <charlswall@live.com>
2023-05-23 13:55:01 +0300

    Added a formSubmit state to capture all form user input

--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -1,92 +1,134 @@
+import {useState} from "react"
+
+
+const signupFormStyle = {
+  "display": "flex",
+  "width": "30%",
+  "border": "1px solid #000",
+  "borderRadius": "20px",
+  "margin": "100px auto",
+  "padding": "30px"
+};
+
 export default function SignUp() {
-  const signupFormStyle = {
-    "display": "flex",
-    "width": "30%",
-    "border": "1px solid #000",
-    "border-radius": "20px",
-    "margin": "100px auto",
-    "padding": "30px"
-  };
+  const [formData, setFormData] = useState({
+    first_name : "",
+    last_name: "",
+    password: "",
+    password_confirmation: "",
+    telephone_no: "",
+    email: "",
+    role: ""
+  })
+
+
+  function handleChange(e){
+    const name = e.target.name
+    const value = e.target.value
+
+    setFormData({
+      ...formData,
+      [name]: value,
+    });
+  }
+
+  function handleSubmit(e){
+    e.preventDefault()
+
+    console.log(formData)
+  }
 
   return (
     <div style={signupFormStyle}>
-      <form class="row g-3">
+      <form className="row g-3" onSubmit={handleSubmit}>
         {/* Names */}
-        <div class="col-md-6">
-          <label for="inputFirstName" class="form-label">
+        <div className="col-md-6">
+          <label for="inputFirstName" className="form-label">
             First Name
           </label>
           <input
             type="text"
-            class="form-control"
+            className="form-control"
             id="inputFirstName"
-            name="firstName"
+            name="first_name"
+            onChange={handleChange}
+            value={formData.first_name}
           />
         </div>
-        <div class="col-md-6">
-          <label for="inputLastName" class="form-label">
+        <div className="col-md-6">
+          <label for="inputLastName" className="form-label">
             Last Name
           </label>
           <input
             type="text"
-            class="form-control"
+            className="form-control"
             id="inputLastName"
-            name="lastName"
+            name="last_name"
+            onChange={handleChange}
+            value={formData.last_name}
           />
         </div>
         {/* Email + Tel */}
-        <div class="col-md-6">
-          <label for="inputEmail" class="form-label">
+        <div className="col-md-6">
+          <label for="inputEmail" className="form-label">
             Email
           </label>
           <input
             type="email"
-            class="form-control"
+            className="form-control"
             id="inputEmail"
             name="email"
+            onChange={handleChange}
+            value={formData.email}
           />
         </div>
-        <div class="col-md-6">
-          <label for="inputTelephone" class="form-label">
+        <div className="col-md-6">
+          <label for="inputTelephone" className="form-label">
             Telephone No
           </label>
           <input
             type="text"
-            class="form-control"
+            className="form-control"
             id="inputTelephone"
-            name="telephoneNo"
+            name="telephone_no"
+            onChange={handleChange}
+            value={formData.telephone_no}
           />
         </div>
 
         {/* Password Confirmation */}
-        <div class="col-md-6">
-          <label for="inputPassword" class="form-label">
+        <div className="col-md-6">
+          <label for="inputPassword" className="form-label">
             Password
           </label>
           <input
             type="password"
-            class="form-control"
+            className="form-control"
             id="inputPassword"
             name="password"
+            onChange={handleChange}
+            value={formData.password}
           />
         </div>
-        <div class="col-md-6">
-          <label for="inputPasswordConfirmation" class="form-label">
+        <div className="col-md-6">
+          <label for="inputPasswordConfirmation" className="form-label">
             Confirm Password
           </label>
           <input
             type="password"
-            class="form-control"
+            className="form-control"
             id="inputPasswordConfirmation"
-            name="passwordConfirmation"
+            name="password_confirmation"
+            onChange={handleChange}
+            value={formData.password_confirmation}
           />
         </div>
         {/* Choose user role */}
-        <div class="col-md-12">
-          <label for="inputRole" class="form-label">
+        <div className="col-md-12">
+          <label for="inputRole" className="form-label">
             I want to:
           </label>
-          <select id="inputRole" class="form-select" name="role">
+          <select id="inputRole" className="form-select" name="role" onChange={handleChange}>
             <option selected disabled>
               Choose...
             </option>
@@ -94,8 +136,8 @@ export default function SignUp() {
             <option value="user">Rent a space</option>
           </select>
         </div>
-        <div class="col-12" style={{"text-align": "center"}}>
-          <button type="submit" class="btn btn-primary">
+        <div className="col-12" style={{"textAlign": "center"}}>
+          <button type="submit" className="btn btn-primary">
             SIGN UP
           </button>
         </div>
