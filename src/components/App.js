4b12608ca0c53978c856591c262d8f70bf2fec2b
Charles Swaleh <charlswall@live.com>
2023-05-23 20:26:52 +0300

    Preparation for merging

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,12 @@
 import "../App.css";
-import Login from "./Login";
 
 function App() {
   return (
-    // <div className="App">
-    //   <header className="App-header">
-    //     <h1>BizzSpace</h1>
-    //   </header>
-    // </div>
-      <Login />
+    <div className="App">
+      <header className="App-header">
+        <h1>BizzSpace</h1>
+      </header>
+    </div>
   );
 }
 
