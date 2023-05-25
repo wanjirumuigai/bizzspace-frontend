f2d799d6fb5eed8c524825645ab2855b06673d15
Pauline Muigai <wanjirumuigai@gmail.com>
2023-05-25 09:44:01 +0300

    edited the Home Component to fetch data and pass it to Search and SpaceContainer as props

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,10 @@
 import React, { useState, useEffect } from "react";
 import SpaceContainer from "./SpaceContainer";
+import Search from "./Search";
 
 function Home() {
+  const [spaces, setSpaces] = useState([]);
+  const [searchItems, setSearchItems] = useState([]);
   const searchStyle = {
     // display: "flex",
     // padding: "20px",
@@ -10,18 +13,37 @@ function Home() {
     color: "black",
   };
 
+  useEffect(() => {
+    fetch("/spaces")
+      .then((res) => res.json())
+      .then((data) => {
+        setSpaces(data);
+      });
+  }, []);
+
+  function handleSearch(e) {
+    const searchMatch = spaces.filter(
+      (item) =>
+        item.name.toLowerCase().includes(e.target.value.toLowerCase()) ||
+        item.location.toLowerCase().includes(e.target.value.toLowerCase())
+    );
+    setSearchItems(searchMatch);
+  }
+  console.log(spaces);
+
   return (
     <div className="card" style={searchStyle}>
       {/* <img src="..." class="card-img-top" alt="..."/>THIS WILL BE BACKGROUND IMAGE */}
       <div className="searchfilterunderlayimage">
         <div className="card-body stylingfiltersearch">
           <h1 id="h1-in-search-div">Discover your next business space</h1>
-          <select name="filter">
+          {/* <select name="filter">
             <option value="All">Filter listings by city</option>
             <option value="Nairobi">Nairobi</option>
             <option value="Kisumu">Kisumu</option>
             <option value="Mombasa">Mombasa</option>
-          </select>
+          </select> */}
+          {/* <Search handleSearch={handleSearch} /> */}
 
           <input
             id="searchinput"
@@ -34,7 +56,7 @@ function Home() {
       </div>
 
       <div className="card-body menu-container">
-        <SpaceContainer />
+        <SpaceContainer spaces={spaces} setSpaces={setSpaces} />
       </div>
     </div>
   );
--- src/components/SpaceContainer.js
+++ src/components/SpaceContainer.js
@@ -2,10 +2,11 @@ import React from "react";
 import SpaceCard from "./SpaceCard";
 import "../App.css";
 
-function SpaceContainer() {
+function SpaceContainer({ spaces, setSpaces }) {
+  console.log(spaces);
   return (
     <ul className="cards">
-      <SpaceCard />
+      <SpaceCard spaces={spaces} setSpaces={setSpaces} />
     </ul>
   );
 }
