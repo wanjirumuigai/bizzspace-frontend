bf9f53ee903b22c57f27b690f2d2895559362d04
Pauline Muigai <wanjirumuigai@gmail.com>
2023-05-29 08:13:01 +0300

    rectified class to className on home component

--- src/components/Home.js
+++ src/components/Home.js
@@ -38,7 +38,7 @@ function Home() {
     return (
       <>
         <h3>Loading ...</h3>
-        <div class="loader">
+        <div className="loader">
           <span id="span"></span>
           <span id="span"></span>
           <span id="span"></span>
